37b29aaa4580e34e5b339403a477f8f6f526eabd
Martin <martin.feuillet92@gmail.com>
2022-09-09 16:09:22 +0200

    commit 1

---
+++ script.js
@@ -0,0 +1,18 @@
+let button = document.querySelector('.clicknow');
+let button2 = document.querySelector('.clicknow2');
+
+// check if cookie exists
+
+
+
+button.addEventListener('click', function() {
+    document.cookie='ingenius-popup = true; expires = 50';
+})
+
+button2.addEventListener('click', function() {
+    if (document.cookie.indexOf('ingenius-popup') >= 0) {
+        alert('cookie exists');
+    }
+})
+
+// if cookie exists, hide popupgi
